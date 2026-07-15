6857302100f31cddbb726f2d271d3faad3fa5cb2
AngeloCG97 <angelocas13_8@hotmail.com>
2026-07-15 11:03:39 -0600

    feat(publish): created workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
   },
 }
 
